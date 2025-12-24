8789c5d3a77a8c11d4660188d173c2ad7eab67ee
abirhf <hochlefabir@gmail.com>
2025-12-24 17:11:44 +0100

    Delete src/main directory

--- src/main/java/com/example/demo/dto/FabricantOrigineDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.dto;
-
-public class FabricantOrigineDTO extends ClientDTO {
-    private String secteur;
-}
--- src/main/java/com/example/demo/repositories/CategorieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.entities.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-
-    Categorie findByNom(String nom);
-}
